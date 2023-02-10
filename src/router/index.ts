71b87089266b55682540f3387d1b8c3f3e10f880
OVYVO <vue12306@163.com>
2023-02-10 15:36:55 +0800

    :zap: perf: router优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import type { AppRouteRecordRaw } from './types'
 
 const routes: AppRouteRecordRaw[] = []
