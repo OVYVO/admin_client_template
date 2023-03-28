2cafd02ee3812190f23ef60b5cf6309331150920
yangang <yang@ktwelve.com.cn>
2023-03-28 13:24:56 +0800

    :bug: fix: fix

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,13 +6,13 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { getUser } from '@/api/common'
+// import { getUser } from '@/api/common'
 
 const visible = ref(true)
 onMounted(async () => {
-  console.log('123')
-  const data = await getUser()
-  console.log(data)
+  // console.log('123')
+  // const data = await getUser()
+  // console.log(data)
 })
 </script>
 
